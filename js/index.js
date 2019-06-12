e475e9403eb40757bc8b36eec91266b19624023e
roadstobe <epro100live@gmail.com>
2019-06-12 22:20:23 +0300

    fix devide

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-let buttonPlus = document.getElementById('buttonPlus');
+﻿let buttonPlus = document.getElementById('buttonPlus');
 let buttonMinus = document.getElementById('buttonMinus');
 let buttonMultyplay = document.getElementById('buttonMultyplay');
 let buttonDevide = document.getElementById('buttonDevide');
@@ -6,46 +6,34 @@ let buttonDevide = document.getElementById('buttonDevide');
 let input1 = document.getElementById('number1');
 let input2 = document.getElementById('number2');
 
-function getNumber(elementId){
-    return elementId.value;
-}
-
-function onButtonPlus(){
-    window.alert(Number(getNumber(input1)) + Number(getNumber(input2)));
-}
-
-function onbuttonMinus(){
-    window.alert(Number(getNumber(input1)) - Number(getNumber(input2)));
-}
-
-function onbuttonMultyplay(){
-    window.alert(Number(getNumber(input1)) * Number(getNumber(input2)));
-}
-
-function onbuttonDevide(){
-    if(Number(getNumber(input2) == 0))
-        window.alert('на нуль ділити не можна');
-    else
-        window.alert(Number(getNumber(input1)) * Number(getNumber(input2)));
-}
-
-
-
-
-
-function fillDate(){
+function makeOperation(operation){
+    let number1 = Number(input1.value);
+    let number2 = Number(input2.value);
+    let result;
+    if(operation === '+')
+        {result = number1 + number2;}
+    else if (operation === '-')
+        result = number1 - number2;
+    else if (operation === '*')
+        result = number1 * number2;
+    else if(number2 === 0)
+        result = 'на нуль ділити не можна'
+    else if(operation === '/' && number2 != 0)
+        result = number1 / number2;
+    else 
+        result = 'operation  is unknown'
     
-    
-    
-     number1 = input1.value;
-     number2 = input2.value;
+    window.alert(result);
 }
 
+function onOperationButton(eventObject){
+    let clickElement = eventObject.currentTarget;
+    let operation = clickElement.value;
+    makeOperation(operation)
+}
 
-number1.addEventListener('keydown', fillDate);
-number2.addEventListener('keydown', fillDate);
-buttonPlus.addEventListener('click', onButtonPlus);
-buttonMinus.addEventListener('click', onbuttonMinus);
-buttonMultyplay.addEventListener('click', onbuttonMultyplay);
-buttonDevide.addEventListener('click', onbuttonDevide);
+buttonPlus.addEventListener('click', onOperationButton);
+buttonMinus.addEventListener('click', onOperationButton);
+buttonMultyplay.addEventListener('click', onOperationButton);
+buttonDevide.addEventListener('click', onOperationButton);
 
